3525fb923e85722fba8277ed0291d82634b1fd9c
Felix Soest <felix@soest-net.de>
2020-09-27 18:32:40 +0200

    Solver

--- code/qut.py
+++ code/qut.py
@@ -2,19 +2,42 @@ import numpy as np
 import qutip as qt
 import matplotlib.pyplot as plt
 from functools import partial
+from scipy.interpolate import CubicSpline
+from scipy.integrate import quad
 
-u_drive = np.sin(np.linspace(0, 2 * np.pi, 21))
-v_drive = np.cos(np.linspace(0, 2 * np.pi, 21))
-w = np.zeros(21)
+u_drive = np.cos(np.linspace(0, 2 * np.pi, 100))
+v_drive = np.zeros(100)
+w = np.sin(np.linspace(0, 2 * np.pi, 100))
+
+theta = np.linspace(0, 2 * np.pi, 100)
+phi = np.pi * np.sin(np.linspace(0, 2 * np.pi, 100))
 
 
-def bloch_to_vec(u, v, w):
-    """
-    """
-    theta = np.arccos(w)
-    phi = np.arcsin(v / np.sin(theta))
+# def bloch_to_vec(u, v, w):
+#     """
+#     """
+#     theta = np.arccos(w)
+#     phi = np.arcsin(v / np.sin(theta))
+#     psi = np.cos(theta / 2) * qt.fock(2, 0) + np.exp(1j * phi) * np.sin(theta / 2) * qt.fock(2, 1)
+#     return psi
+
+
+def bloch_to_vec(theta, phi):
     psi = np.cos(theta / 2) * qt.fock(2, 0) + np.exp(1j * phi) * np.sin(theta / 2) * qt.fock(2, 1)
     return psi
+
+
+def vec_derivative(theta, phi, dtheta, dphi):
+    """
+    """
+    dpsi_dt = -1 * np.sin(theta / 2) * dtheta * qt.fock(2, 0) \
+        + np.exp(1j * phi) * (np.cos(theta / 2) * dtheta + \
+        1j * dphi * np.sin(theta / 2)) * qt.fock(2, 1)
+    return dpsi_dt
+
+
+# for i in range(100):
+#     print(bloch_to_vec(u_drive[i], v_drive[i], w[i]))
 
 
 def rho(psi):
@@ -23,8 +46,8 @@ def rho(psi):
     return psi * psi.dag()
 
 
-s_plus = qt.create(2)                     # Creation operator
-s_minus = s_plus.dag()                    # Annihilation operator
+s_plus = qt.sigmap()                     # Creation operator
+s_minus = qt.sigmam()                    # Annihilation operator
 
 # Interaction Hamiltonian
 H_i = qt.tensor(s_plus, s_minus) + qt.tensor(s_minus, s_plus)
@@ -32,41 +55,56 @@ H_i = qt.tensor(s_plus, s_minus) + qt.tensor(s_minus, s_plus)
 # DST Hamiltonian
 H_dst = qt.tensor(H_i, qt.qeye(2)) + qt.tensor(qt.qeye(2), H_i)
 
-# H_s = (H_dst * qt.tensor(rho(psi_d(3)), qt.qeye(2), rho(psi_d(np.pi)))).ptrace(1)
-# H_s
-
-t = np.linspace(0, 10, 500)
+t = np.linspace(0, 10, 100)
 
 
 # %%
 # Evolution of state vector
+# u_d = qt.Cubic_Spline(t[0], t[-1], u_drive)
+# v_d = qt.Cubic_Spline(t[0], t[-1], v_drive)
+# w_d = qt.Cubic_Spline(t[0], t[-1], w)
+u_d = CubicSpline(t, u_drive)
+v_d = CubicSpline(t, v_drive)
+w_d = CubicSpline(t, w)
 
-u_d = qt.Cubic_Spline(t[0], t[-1], u_drive)
-v_d = qt.Cubic_Spline(t[0], t[-1], v_drive)
-w_d = qt.Cubic_Spline(t[0], t[-1], w)
-qdrive = [u_d, v_d, w_d]
+theta_d = CubicSpline(t, theta)
+phi_d = CubicSpline(t, phi)
+
+# drive = [u_d, v_d, w_d]
+drive = [theta_d, phi_d]
 transducer = drive
 
-
-def H_s(t, drive=qdrive, transducer=qdrive, H_dst=H_dst):
+def H_s(t, args):
     """
     """
-    psi_d = bloch_to_vec(drive[0](t), drive[1](t), drive[2](t))
-    psi_t = bloch_to_vec(transducer[0](t), transducer[1](t), transducer[2](t))
+    drive, transducer, H_dst = args
+    # psi_d = bloch_to_vec(drive[0](t), drive[1](t), drive[2](t))
+    # psi_t = bloch_to_vec(transducer[0](t), transducer[1](t), transducer[2](t))
+    psi_d = bloch_to_vec(drive[0](t), drive[1](t))
+    psi_t = bloch_to_vec(transducer[0](t), transducer[1](t))
     H_s = (H_dst * qt.tensor(rho(psi_d), qt.qeye(2), rho(psi_t))).ptrace(1)
     return H_s
 
+res = qt.sesolve(H_s, qt.fock(2, 0), t, args=[drive, transducer, H_dst])
 
-H_evo = partial(H_s, drive=drive, transducer=transducer, H_dst=H_dst)
+# %%
+b = qt.Bloch()
+b.add_states(res.states)
+b.show()
+# %%
 
+def P(t, args):
+    """
+    """
+    drive, system, transducer, H_dst = args
+    psi_d = bloch_to_vec(drive[0](t), drive[1](t))#, drive[2](t))
+    psi_t = bloch_to_vec(transducer[0](t), transducer[1](t))#, transducer[2](t))
+    # time derivative of psi_t
+    dpsi_dt = vec_derivative(transducer[0](t), transducer[1](t), transducer[0].derivative()(t), transducer[1].derivative()(t))
+    bra = qt.tensor(psi_d, system[t], dpsi_dt).dag()
+    ket = qt.tensor(psi_d, system[t], psi_t)
+    scalar = bra * 1j * H_dst * ket
+    p = 2 * scalar.full()[0].imag
+    return p
 
-args = {
-    "drive": drive,
-    "transducer": transducer,
-    "H_dst": H_dst
-    }
-
-
-# qt.sesolve(H_s, qt.fock(2, 0), t, args=args)
-
-qt.sesolve(H_s, qt.fock(2, 0), t)
+args = drive, res.states, transducer, H_dst
---
+++ code/solver.py
@@ -0,0 +1,92 @@
+import numpy as np
+import qutip as qt
+from scipy.integrate import solve_ivp
+from scipy.interpolate import CubicSpline
+import matplotlib.pyplot as plt
+
+
+def bloch_to_vec(theta, phi):
+    """
+    """
+    psi = np.cos(theta / 2) * qt.fock(2, 0) + np.exp(1j * phi) * np.sin(theta / 2) * qt.fock(2, 1)
+    return psi
+
+
+def vec_derivative(theta, phi, dtheta, dphi):
+    """
+    """
+    dpsi_dt = -1 * np.sin(theta / 2) * dtheta * qt.fock(2, 0) \
+        + np.exp(1j * phi) * (np.cos(theta / 2) * dtheta + \
+        1j * dphi * np.sin(theta / 2)) * qt.fock(2, 1)
+    return dpsi_dt
+
+
+def rho(psi):
+    """
+    """
+    return psi * psi.dag()
+
+
+def rhs(t, y, t_arr, t_drive, p_drive, t_transducer, p_transducer, H_dst):
+    """
+    t: time
+    y: array, [E, |psi_s>]
+    """
+    # Spline functions
+    theta_d = CubicSpline(t_arr, t_drive)
+    phi_d = CubicSpline(t_arr, p_drive)
+    theta_t = CubicSpline(t_arr, t_transducer)
+    phi_t = CubicSpline(t_arr, p_transducer)
+
+    # System hamiltonian
+    psi_d = bloch_to_vec(theta_d(t), phi_d(t))  # Qobj
+    psi_s = y[1:]                         # Array!!!
+    psi_t = bloch_to_vec(theta_t(t), phi_t(t))  # Qobj
+    H_s = (H_dst * qt.tensor(rho(psi_d), qt.qeye(2), rho(psi_t))).ptrace(1)
+
+    # Evolution
+    dpsi_s_dt = -1j * H_s.full() @ psi_s
+
+    # Time derivative of transducer state
+    dpsi_t_dt = vec_derivative(theta_t(t), phi_t(t), theta_t.derivative()(t), \
+        phi_t.derivative()(t))
+
+    # Overlap
+    bra = qt.tensor(psi_d, qt.Qobj(psi_s), dpsi_t_dt).dag()
+    ket = qt.tensor(psi_d, qt.Qobj(psi_s), psi_t)
+    scalar = bra * -1j * H_dst * ket
+    p = -2 * scalar.full()[0].imag
+    res = np.full(3, p, dtype=np.complex)
+    res[1:] = dpsi_s_dt
+    return res
+
+
+# %%
+# Interaction Hamiltonian
+H_i = qt.tensor(qt.sigmap(), qt.sigmam()) + qt.tensor(qt.sigmam(), qt.sigmap())
+# DST Hamiltonian
+H_dst = qt.tensor(H_i, qt.qeye(2)) + qt.tensor(qt.qeye(2), H_i)
+
+theta = np.linspace(0, 2 * np.pi, 100)
+phi = np.pi * np.sin(np.linspace(0, 2 * np.pi, 100))
+
+t_arr = np.linspace(0, 10, 100)
+
+args = (t_arr, phi, phi, theta, theta, H_dst)
+
+t_span = (0, 10)
+
+# rhs(0.5, np.array([0, 1+0j, 0+0j]), *args)
+
+E = solve_ivp(rhs, t_span, np.array([0, 1/np.sqrt(2) + 0j, 1/np.sqrt(2) + 0j]), args=args)
+
+# %%
+states = []
+b = qt.Bloch()
+
+for i in range(len(E.y[1:].T)):
+    b.add_states(qt.Qobj(E.y[1:].T[i]))
+b.show()
+
+# %%
+plt.plot(E.t, E.y[0])
