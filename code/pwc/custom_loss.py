d9da32d39738647d4a78cf259aa396619ec590f7
fsoest <felix@soest-net.de>
2021-01-17 14:53:33 +0100

    Custom loss

--- code/pwc/custom_loss.py
+++ code/pwc/custom_loss.py
@@ -41,13 +41,8 @@ def matexp(x, dt):
     return exp
 
 
-def real_matmul(A, B):
-    A_real = torch.real(A)
-    A_imag = torch.imag(A)
-    B_real = torch.real(B)
-    B_imag = torch.imag(B)
-
-    return A_real @ B_real - A_imag @ B_imag + 1j * (A_imag @ B_real + A_real @ B_imag)
+def real_matmul(A_real, A_imag, B_real, B_imag):
+    return A_real @ B_real - A_imag @ B_imag, A_imag @ B_real + A_real @ B_imag
 
 
 class work_loss():
@@ -60,24 +55,38 @@ class work_loss():
 
     def forward(self, y, x):
         # Calculate rho_0
-        # Dimensions: [Batch, Time, Embedding]
         with torch.no_grad():
+            # Dimensions: [Batch, Time, Embedding]
             sin_phi_D = x[:, :self.N - 1, 1]
             cos_phi_D = x[:, :self.N - 1, 3]
             exp_phi_D = cos_phi_D + 1j * sin_phi_D
-            rho_0 = torch.full((x.shape[0], 2, 2), 0.5, dtype=torch.cdouble)
-            rho_0[:, 1, 0] *= exp_phi_D[:, 0]
-            rho_0[:, 0, 1] *= torch.conj(exp_phi_D[:, 0])
+            rho_0_real = torch.full((x.shape[0], 2, 2), 0.5)
+            rho_0_imag = torch.zeros((x.shape[0], 2, 2))
+            rho_0_real[:, 1, 0] *= cos_phi_D[:, 0]
+            rho_0_real[:, 0, 1] *= cos_phi_D[:, 0]
+            rho_0_imag[:, 1, 0] = 0.5 * sin_phi_D[:, 0]
+            rho_0_imag[:, 0, 1] = -0.5 * sin_phi_D[:, 0]
 
         # Calculate unitary evolution operators
 
         # No grad for drive side
         with torch.no_grad():
             # H_D: [batch, time, 2x2 matrix]
-            H_D = torch.zeros((x.shape[0], self.N - 1, 2, 2), dtype=torch.cdouble)
             sin_theta_D = x[:, :self.N - 1, 0]
+
+            H_D = torch.zeros((x.shape[0], self.N - 1, 2, 2), dtype=torch.cdouble)
+
             H_D[:, :, 1, 0] = exp_phi_D * sin_theta_D / 2
             H_D[:, :, 0, 1] = torch.conj(exp_phi_D) * sin_theta_D / 2
+
+            H_D_real = torch.zeros((x.shape[0], self.N - 1, 2, 2))
+            H_D_imag = torch.zeros((x.shape[0], self.N -1, 2, 2))
+
+            H_D_real[:, :, 1, 0] = 0.5 * sin_theta_D * cos_phi_D
+            H_D_real[:, :, 0, 1] = 0.5 * sin_theta_D * cos_phi_D
+
+            H_D_imag[:, :, 1, 0] = 0.5 * sin_theta_D * sin_phi_D
+            H_D_imag[:, :, 0, 1] = -0.5 * sin_theta_D * sin_phi_D
 
 
         # H_T: [batch, time, 2x2]
@@ -88,20 +97,59 @@ class work_loss():
         H_T[:, :, 1, 0] = (torch.cos(phi_T) + 1j * torch.sin(phi_T)) * torch.sin(theta_T) / 2
         H_T[:, :, 0, 1] = (torch.cos(phi_T) - 1j * torch.sin(phi_T)) * torch.sin(theta_T) / 2
 
+        H_T_real = torch.zeros((y.shape[0], self.N, 2, 2))
+        H_T_imag = torch.zeros((y.shape[0], self.N, 2, 2))
+
+        H_T_real[:, :, 1, 0] = torch.cos(phi_T) * torch.sin(theta_T) / 2
+        H_T_real[:, :, 0, 1] = torch.cos(phi_T) * torch.sin(theta_T) / 2
+
+        H_T_imag[:, :, 1, 0] = torch.sin(phi_T) * torch.sin(theta_T) / 2
+        H_T_imag[:, :, 0, 1] = -1 * torch.sin(phi_T) * torch.sin(theta_T) / 2
+
+
+        # Abs value of alpha = delta + tau
+        # shape: [batch, 2]
+        abs_alpha = torch.sqrt(torch.pow(torch.sin(theta_T[:, :2]), 2) + torch.pow(sin_theta_D[:, :2], 2) + 2 * torch.sin(theta_T[:, :2]) * sin_theta_D[:, :2] * (torch.cos(phi_T[:, :2]) * cos_phi_D[:, :2] + torch.sin(phi_T[:, :2]) * sin_phi_D[:, :2])) / 2
+
+        alpha_real = 0.5 * (sin_theta_D[:, :2] * cos_phi_D[:, :2] + torch.sin(theta_T[:, :2]) * torch.cos(phi_T[:, :2]))
+        alpha_imag = 0.5 * (sin_theta_D[:, :2] * sin_phi_D[:, :2] + torch.sin(theta_T[:, :2]) * torch.sin(phi_T[:, :2]))
+
+        # Unitary evolution operator
+        # Shape: [batch, 2, 2, 2]
+
+
+        U_real = torch.zeros((x.shape[0], 2, 2, 2))
+        U_imag = torch.zeros((x.shape[0], 2, 2, 2))
+
+        # Helpers
+        c = torch.cos(abs_alpha * self.dt)
+        s = torch.div(torch.sin(abs_alpha * self.dt), abs_alpha)
+
+        U_real[:, :, 0, 0] = c
+        U_real[:, :, 1, 1] = c
+        U_real[:, :, 0, 1] = torch.mul(-1 * alpha_imag, s)
+        U_real[:, :, 1, 0] = torch.mul(alpha_imag, s)
+
+        U_imag[:, :, 0, 1] = torch.mul(-1 * alpha_real, s)
+        U_imag[:, :, 1, 0] = torch.mul(-1 * alpha_real, s)
+
+
         U_1 = matexp(H_D[:, 0] + H_T[:, 0], self.dt)
         U_2 = matexp(H_D[:, 1] + H_T[:, 1], self.dt)
 
-        helper = real_matmul(U_1, rho_0)
-        rho_1 = real_matmul(helper, torch.transpose(U_1, 1, 2).conj())
+        # helper = real_matmul(U_1, rho_0)
 
-        A_1 = real_matmul(rho_1, (H_T[:, 1] - H_T[:, 0]))
-        W_1 = torch.real(A_1[:, 0, 0] + A_1[:, 1, 1])
+        helper_real, helper_imag = real_matmul(U_real[:, 0], U_imag[:, 0], rho_0_real, rho_0_imag)
+        rho_1_real, rho_1_imag = real_matmul(helper_real, helper_imag, torch.transpose(U_real[:, 0], 1, 2), -1 * torch.transpose(U_imag[:, 0], 1, 2))
 
-        help2 = real_matmul(U_2, rho_1)
-        rho_2 = real_matmul(help2, torch.transpose(U_2, 1, 2).conj())
+        A_1_real, A_1_imag = real_matmul(rho_1_real, rho_1_imag, (H_T_real[:, 1] - H_T_real[:, 0]), (H_T_imag[:, 1] - H_T_imag[:, 0]))
+        W_1 = A_1_real[:, 0, 0] + A_1_real[:, 1, 1]
 
-        A_2 = real_matmul(rho_2, (H_T[:, 2] - H_T[:, 1]))
-        W_2 = torch.real(A_2[:, 0, 0] + A_2[:, 1, 1])
+        help2_real, help2_imag = real_matmul(U_real[:, 1], U_imag[:, 1], rho_1_real, rho_1_imag)
+        rho_2_real, rho_2_imag = real_matmul(help2_real, help2_imag, torch.transpose(U_real[:, 1], 1, 2), torch.transpose(-1 * U_imag[:, 1], 1, 2))
+
+        A_2_real, A_2_imag = real_matmul(rho_2_real, rho_2_imag, H_T_real[:, 2] - H_T_real[:, 1], H_T_imag[:, 2] - H_T_imag[:, 1])
+        W_2 = A_2_real[:, 0, 0] + A_2_real[:, 1, 1]
 
         return torch.mean(W_1 + W_2)
 
@@ -190,7 +238,7 @@ class LSTMNetwork(nn.Module):
             if valid_loss < loss_high:
                 loss_high = valid_loss
                 count = 0
-                torch.save(self, 'best_model_{}'.format(self.dropout))
+                torch.save(self, 'best_model_custom_loss')
                 print('Now! Loss: {}'.format(self.calc_loss(valid_set)))
             else:
                 count += 1
@@ -272,7 +320,7 @@ def train_lstm_total_dropout(dropout, learning_rate, patience, batch_size, n_lay
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimiser, mode='min', factor=sched_factor, patience=patience/pat_drop)
     epoch = model.learn(train_set, valid_set, optimiser, scheduler, patience=patience)
 
-    model = torch.load('best_model_{}'.format(dropout)).eval()
+    model = torch.load('best_model_custom_loss').eval()
     vloss = model.calc_loss(valid_set)
     vwork = model.work_ratio(data_test, dt)
     return epoch, vloss, vwork
@@ -300,7 +348,7 @@ seed = 42
 rho = 'eigen'
 net = 'custom_loss'
 N_sobol = 1
-N_x = 1000
+N_x = 30000
 
 
 if __name__ == '__main__':
