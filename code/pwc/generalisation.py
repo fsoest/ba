78dd56e5f942b608c3ac9f4d530d5bd6e5a9fe8e
fsoest <felix@soest-net.de>
2021-02-01 20:24:00 +0100

    Generalisation

--- code/pwc/generalisation.py
+++ code/pwc/generalisation.py
@@ -32,9 +32,12 @@ for i in range(N_data):
 rho_0 = get_eigen_rho(thetas[:, 0], phis[:, 0])
 # %%
 # Make predictions and calculate work outputs
-uni = torch.load('models/dt_1_uni').eval()
-bi = torch.load('models/dt_1_bi').eval()
-uni_local = torch.load('models/dt_1_bi').eval()
+uni = torch.load('models/dt_5_uni').eval()
+uni = torch.load('models/N_5_rho_eigen_lstm_uni').eval()
+bi = torch.load('models/dt_5_bi').eval()
+bi = torch.load('models/N_5_rho_eigen_lstm').eval()
+uni_local = torch.load('models/local_opt/dt5_uni').eval()
+better = torch.load('models/custom_loss_dt_1_better').eval()
 # %%
 N = 5
 seed = 42
@@ -45,12 +48,12 @@ N_sobol = 45
 runs = range(21)
 
 # %%
-data = import_datasets('multi_train_data', N, dt, rho, N_sobol, runs)
-data_train, data_test = train_test_split(data, test_size=0.18, random_state=seed)
-data_train, data_valid = train_test_split(data_train, test_size=0.1, random_state=seed)
-train_set = WorkDataset(data_train, N, net='lstm')
-test_set = WorkDataset(data_test, N, net='lstm')
-valid_set = WorkDataset(data_valid, N, net='lstm')
+# data = import_datasets('multi_train_data', N, dt, rho, N_sobol, runs)
+# data_train, data_test = train_test_split(data, test_size=0.18, random_state=seed)
+# data_train, data_valid = train_test_split(data_train, test_size=0.1, random_state=seed)
+# train_set = WorkDataset(data_train, N, net='lstm')
+# test_set = WorkDataset(data_test, N, net='lstm')
+# valid_set = WorkDataset(data_valid, N, net='lstm')
 
 # %%
 
@@ -59,6 +62,7 @@ E_uni = np.zeros((N_data, N_max, N_max))
 E_bi = np.zeros((N_data, N_max, N_max))
 E_triv = np.zeros((N_data, N_max, N_max))
 E_local = np.zeros((N_data, N_max, N_max))
+E_better = np.zeros((N_data, N_max, N_max))
 # %%
 for n in N_arr:
     x_embed = angle_embedding(np.concatenate((thetas[:, :n], phis[:, :n]), axis=1), n)
@@ -66,37 +70,58 @@ for n in N_arr:
     uni.N = n
     bi.N = n
     with torch.no_grad():
-        hidden_uni, cell_uni = uni.HiddenCellTest(N_data)
-        hidden_bi, cell_bi = bi.HiddenCellTest(N_data)
-        hidden_local, cell_local = uni_local.HiddenCellTest(N_data)
-        y_uni = uni(x_embed, hidden_uni, cell_uni)[0]
-        y_bi = bi(x_embed, hidden_bi, cell_bi)[0]
-        y_local = uni_local(x_embed, hidden_local, cell_local)[0]
-    y_uni = rev_angle_embedding(y_uni.detach().numpy(), n)
-    y_bi = rev_angle_embedding(y_bi.detach().numpy(), n)
-    y_local = rev_angle_embedding(y_local.detach().numpy(), n)
+        # hidden_uni, cell_uni = uni.HiddenCellTest(N_data)
+        # hidden_bi, cell_bi = bi.HiddenCellTest(N_data)
+        # hidden_local, cell_local = uni_local.HiddenCellTest(N_data)
+
+        h_better, c_better = better.HiddenCellTest(N_data)
+        # y_uni = uni(x_embed, hidden_uni, cell_uni)[0]
+        # y_bi = bi(x_embed, hidden_bi, cell_bi)[0]
+        # y_local = uni_local(x_embed, hidden_local, cell_local)[0]
+
+        y_better = better(x_embed, h_better, c_better)[0]
+
+    # y_uni = rev_angle_embedding(y_uni.detach().numpy(), n)
+    # y_bi = rev_angle_embedding(y_bi.detach().numpy(), n)
+    # y_local = rev_angle_embedding(y_local.detach().numpy(), n)
+
+    y_better = rev_angle_embedding(y_better.detach().numpy(), n)
     # Calculate energies
     for i in range(N_data):
-        E_uni[i, n-2, :n-1] = rho_path(thetas[i, :n], phis[i, :n], y_uni[i, :n], y_uni[i, n:], dt, rho_0[i], n, 1)[2][:-1]
-        E_bi[i, n-2, :n-1] = rho_path(thetas[i, :n], phis[i, :n], y_bi[i, :n], y_bi[i, n:], dt, rho_0[i], n, 1)[2][:-1]
-        E_local[i, n-2, :n-1] = rho_path(thetas[i, :n], phis[i, :n], y_local[i, :n], y_local[i, n:], dt, rho_0[i], n, 1)[2][:-1]
-        a = lower_bound(np.concatenate((thetas[i, :n], phis[i, :n])), n, dt)
-        E_triv[i, n-2, :n-1] = a[0][:-1]
+        # E_uni[i, n-2, :n-1] = rho_path(thetas[i, :n], phis[i, :n], y_uni[i, :n], y_uni[i, n:], dt, rho_0[i], n, 1)[2][:-1]
+        # E_bi[i, n-2, :n-1] = rho_path(thetas[i, :n], phis[i, :n], y_bi[i, :n], y_bi[i, n:], dt, rho_0[i], n, 1)[2][:-1]
+        # E_local[i, n-2, :n-1] = rho_path(thetas[i, :n], phis[i, :n], y_local[i, :n], y_local[i, n:], dt, rho_0[i], n, 1)[2][:-1]
+        # a = lower_bound(np.concatenate((thetas[i, :n], phis[i, :n])), n, dt)
+        # E_triv[i, n-2, :n-1] = a[0][:-1]
+
+        E_better[i, n-2, :n-1] = rho_path(thetas[i, :n], phis[i, :n], y_better[i, :n], y_better[i, n:], dt, rho_0[i], n, 1)[2][:-1]
 
 
 
-np.save('gen/E_bi_dt_1', E_bi)
-np.save('gen/E_uni_dt_1', E_uni)
-np.save('gen/E_triv_dt_1', E_triv)
+# np.save('gen/E_bi_dt_{0}'.format(dt), E_bi)
+# np.save('gen/E_uni_dt_{0}'.format(dt), E_uni)
+# np.save('gen/E_triv_dt_{0}'.format(dt), E_triv)
+# np.save('gen/E_local_dt_{0}'.format(dt), E_local)
+# # %%
+dt = 5
+E_bi = np.load('gen/E_bi_dt_5.npy')
+E_uni = np.load('gen/E_uni_dt_5.npy')
+E_triv = np.load('gen/E_triv_dt_5.npy')
+E_local = np.load('gen/E_local_dt_5.npy')
+
 # %%
 # Visualisation
 E_uni_sum = np.mean(np.cumsum(E_uni, axis=2)[:, :, -1], axis=0)
 E_bi_sum = np.mean(np.cumsum(E_bi, axis=2)[:, :, -1], axis=0)
 E_triv_sum = np.mean(np.cumsum(E_triv, axis=2)[:, :, -1], axis=0)
-plt.scatter(N_arr[:10], -1 * E_uni_sum[:10], label='Unidir. LSTM')
-plt.scatter(N_arr[:10], -1 * E_bi_sum[:10], label='Bidir. LSTM')
-plt.scatter(N_arr[:10], -1 * E_triv_sum[:10], label='Local opt.')
+E_local_sum = np.mean(np.cumsum(E_local, axis=2)[:, :, -1], axis=0)
+E_better_sum = np.mean(np.cumsum(E_better, axis=2)[:, :, -1], axis=0)
+plt.scatter(N_arr, -1 * E_uni_sum, label='Unidir. LSTM', marker='.')
+plt.scatter(N_arr, -1 * E_bi_sum, label='Bidir. LSTM', marker='.')
+plt.scatter(N_arr, -1 * E_triv_sum, label='Local opt.', marker='.')
+plt.scatter(N_arr, -1 * E_local_sum, label='Local LSTM', marker='.')
+plt.scatter(N_arr, -1 * E_better_sum, label='Better', marker='.')
 plt.legend()
 plt.xlabel('$N$')
-plt.ylabel('$W$')
-plt.savefig('/home/fsoest/ba/phystex/img/gen_dt_1.png')
+plt.ylabel('$\overline{W}$')
+plt.savefig('/home/fsoest/ba/phystex/img/gen_dt_{0}.png'.format(dt), dpi=300)
